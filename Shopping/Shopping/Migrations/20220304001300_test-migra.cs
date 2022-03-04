2d6a15b51b76d66ac3816db3959a258da81ea360
miller ossa samboni <miller.ossa@sofka.com.co>
2022-03-04 16:22:45 -0500

    add create, delet, edit for category

---
+++ Shopping/Shopping/Migrations/20220304001300_test-migra.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shopping.Migrations
+{
+    public partial class testmigra : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_countries",
+                table: "countries");
+
+            migrationBuilder.RenameTable(
+                name: "countries",
+                newName: "Countries");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_countries_Name",
+                table: "Countries",
+                newName: "IX_Countries_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Countries",
+                table: "Countries",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Countries",
+                table: "Countries");
+
+            migrationBuilder.RenameTable(
+                name: "Countries",
+                newName: "countries");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Countries_Name",
+                table: "countries",
+                newName: "IX_countries_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_countries",
+                table: "countries",
+                column: "Id");
+        }
+    }
+}
